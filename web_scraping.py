24263343335b156a00cc7982f62c3427b69c4a3e
YashBhatia97 <iit2015105@iiita.ac.in>
2019-01-04 23:57:24 +0530

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,35 +1,14 @@
-# import urllib2
-# from bs4 import BeautifulSoup
-# wiki = "http://www.fallingrain.com/world/IN/01/"
-
-
-# page = urllib2.urlopen(wiki)
-# soup = BeautifulSoup(page, 'html.parser')
-# all_links = soup.find_all("a")
-# # for link in all_links:
-# # 	print link.get("href")
-# all_tables = soup.find_all('table')
-# for table in all_tables:
-# 	print table
-# # all_tables = all_tables[1]
-# # for row in all_tables.find_all("tr"):
-# # 	for cells in row.find_all('td'):
-# # 		print cells.get_text(),
-# # 	print
-
 import requests
 from bs4 import BeautifulSoup
 import csv
-cities_data = []
+cities_data = [] #dictionary used to save details about city
+
+#Function used to extract data out of table present in the link
 def extracting(link):
 	try:
 		page = requests.get(link)
 		soup = BeautifulSoup(page.content,'html.parser')
 		soup.prettify()
-		#all_links = soup.find_all("a")
-		# for link in all_links:
-		#  	print link.get("href")
-		#  	cnt = cnt + 1
 		selected_table = []
 		all_tables = soup.find_all('table')
 		if len(all_tables)==2:
@@ -49,24 +28,12 @@ def extracting(link):
         			csvwriter = csv.writer(csvfile,  delimiter=',')
         			csvwriter.writerow([data_list[0].text, data_list[2].text, data_list[4].text, data_list[5].text, data_list[6].text, data_list[7].text])
         			print ("Points written sucessfully to file")
-				# cities_data.append({'Name': data_list[0].text,
-    #                             'State': data_list[2].text,
-    #                             'latitude': data_list[4].text,
-    #                             'Longitude': data_list[5].text,
-    #                             'Elevation': data_list[6].text,
-    #                             'Population': data_list[7].text
-    #                             })
-				# for cells in row.find_all('td'):
-				# 	print (cells.get_text()+',', sep=' ')																																																																																						
-				# if cnt != 0:
-				# 	print
-				# else:
-				# 	cnt = 1
 	except:
 		print ("Page Unresponsive....")
 
-links = []
+links = [] #To store all the links that contain information in form of end tables
 
+#Function to find the links of webpages containing data in form of tables
 def findlinks(link):
 	#print link
 	try:
